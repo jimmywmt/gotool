72b836ce12e28ba7b8ba4020963cd646d6c73b2e
Jimmy Ming-Tai Wu <wmt@wmt35.idv.tw>
2022-02-15 15:21:53 +0800

    add Slackwebhook struct and funcs

---
+++ slackwebhook.go
@@ -0,0 +1,44 @@
+package gotool
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Slackwebhook struct {
+	url string
+}
+
+type SlackRequestBody struct {
+	Text string `json:"text"`
+}
+
+func NewSlackWebHook(url string) *Slackwebhook {
+	return &Slackwebhook{url: url}
+}
+func (slackwebhook *Slackwebhook) SentMessage(message string) {
+	slackBody, _ := json.Marshal(SlackRequestBody{Text: message})
+	req, err := http.NewRequest("POST", slackwebhook.url, bytes.NewBuffer(slackBody))
+	if err != nil {
+		log.WithError(err).Warnln("connection failed")
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.WithError(err).Warnln("connection failed")
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	if buf.String() != "ok" {
+		log.WithError(err).Warnln("non-ok response returned from Slack")
+	}
+	defer resp.Body.Close()
+}
